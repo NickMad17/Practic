fd6fa56e496fc81e056f18ba80736b7301ea0c80
Maady <104986153+NickMad17@users.noreply.github.com>
2023-05-18 00:17:18 +0300

    yolu

--- practic/practic.ino
+++ practic/practic.ino
@@ -1,82 +1,64 @@
-#include <LiquidCrystal_I2C.h>
-
 #include <Wire.h>
-#include "Kalman.h"
+#include <math.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-Kalman kalmanX;
-Kalman kalmanY;
-uint8_t IMUAddress = 0x68;
-/* IMU Data */
-int16_t accX;
-int16_t accY;
-int16_t accZ;
-int16_t tempRaw;
-int16_t gyroX;
-int16_t gyroY;
-int16_t gyroZ;
-double accXangle; // Angle calculate using the accelerometer
-double accYangle;
-double temp;
-double gyroXangle = 180; // Angle calculate using the gyro
-double gyroYangle = 180;
-double compAngleX = 180; // Calculate the angle using a Kalman filter
-double compAngleY = 180;
-double kalAngleX; // Calculate the angle using a Kalman filter
-double kalAngleY;
-uint32_t timer;
+const int MPU_addr = 0x68; // Адрес MPU-6050
+
+int16_t accelerometerX, accelerometerY, accelerometerZ; // Переменные для хранения значений ускорения
+
+float sumacc;
+
 void setup() {
-  lcd.init();        
-  lcd.backlight();
+  Wire.begin(); // Инициализация шины I2C
+  Serial.begin(9600); // Инициализация монитора серийного порта
 
-  Wire.begin();
-  Serial.begin(9600);
-  i2cWrite(0x6B,0x00); // Disable sleep mode      
-  kalmanX.setAngle(180); // Set starting angle
-  kalmanY.setAngle(180);
-  timer = micros();
+  // Установка режима активации акселерометра
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  delay(100); // Задержка для стабилизации
 }
+
 void loop() {
-  /* Update all the values */
-  uint8_t* data = i2cRead(0x3B,14);
-  accX = ((data[0] << 8) | data[1]);
-  accY = ((data[2] << 8) | data[3]);
-  accZ = ((data[4] << 8) | data[5]);
-  tempRaw = ((data[6] << 8) | data[7]);
-  gyroX = ((data[8] << 8) | data[9]);
-  gyroY = ((data[10] << 8) | data[11]);
-  gyroZ = ((data[12] << 8) | data[13]);
-  /* Calculate the angls based on the different sensors and algorithm */
-  accYangle = (atan2(accX,accZ)+PI)*RAD_TO_DEG;
-  accXangle = (atan2(accY,accZ)+PI)*RAD_TO_DEG;  
-  double gyroXrate = (double)gyroX/131.0;
-  double gyroYrate = -((double)gyroY/131.0);
-  gyroXangle += kalmanX.getRate()*((double)(micros()-timer)/1000000); // Calculate gyro angle using the unbiased rate
-  gyroYangle += kalmanY.getRate()*((double)(micros()-timer)/1000000);
-  kalAngleX = kalmanX.getAngle(accXangle, gyroXrate, (double)(micros()-timer)/1000000); // Calculate the angle using a Kalman filter
-  kalAngleY = kalmanY.getAngle(accYangle, gyroYrate, (double)(micros()-timer)/1000000);
-  timer = micros();
+  // Чтение значений ускорения по осям X, Y и Z
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B); // Адрес регистра начала чтения данных акселерометра
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 6, true);
 
-  lcd.setCursor(0,0);
-  lcd.print("X:");
-  lcd.print(kalAngleX);
-  lcd.setCursor(0,1);
-  lcd.print("Y:");
-  lcd.print(kalAngleY);
-}
-void i2cWrite(uint8_t registerAddress, uint8_t data){
-  Wire.beginTransmission(IMUAddress);
-  Wire.write(registerAddress);
-  Wire.write(data);
-  Wire.endTransmission(); // Send stop
-}
-uint8_t* i2cRead(uint8_t registerAddress, uint8_t nbytes) {
-  uint8_t data[nbytes];
-  Wire.beginTransmission(IMUAddress);
-  Wire.write(registerAddress);
-  Wire.endTransmission(false); // Don't release the bus
-  Wire.requestFrom(IMUAddress, nbytes); // Send a repeated start and then release the bus after reading
-  for(uint8_t i = 0; i < nbytes; i++)
-    data [i]= Wire.read();
-  return data;
+  accelerometerX = Wire.read() << 8 | Wire.read();
+  accelerometerY = Wire.read() << 8 | Wire.read();
+  accelerometerZ = Wire.read() << 8 | Wire.read();
+
+  // Преобразование значений в g
+  float accelerationX = accelerometerX;
+  float accelerationY = accelerometerY;
+  float accelerationZ = accelerometerZ;
+
+  sumacc = sqrt(pow(accelerationX,2)+pow(accelerationY,2)+pow(accelerationZ,2))/ 14600;
+
+  // // Вывод значений ускорения
+  // Serial.print("Ускорение X: ");
+  // Serial.print(accelerationX);
+  // Serial.print(" g ");
+  // Serial.println(" ");
+
+  // Serial.print("Ускорение Y: ");
+  // Serial.print(accelerationY);
+  // Serial.print(" g ");
+  // Serial.print(" ");
+
+  // Serial.print("Ускорение Z: ");
+  // Serial.print(accelerationZ);
+  // Serial.println(" g ");
+
+  // Serial.print("Общее ускорение: ");
+  Serial.println(sumacc);
+  // Serial.println(" g ");
+
+
+
+
+
 }
